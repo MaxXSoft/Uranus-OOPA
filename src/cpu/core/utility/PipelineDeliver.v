06e86ef4b69f3904626c9582ee9be5d6fa9f4177
MaxXSoft <x@MaxXSoft.net>
2019-05-12 12:23:04 +0800

    (PipelineDeliver) added module 'PipelineDeliver'

---
+++ src/cpu/core/utility/PipelineDeliver.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module PipelineDeliver #(parameter WIDTH = 1) (
+    input                   clk,
+    input                   rst,
+    input                   flush,
+    input                   stall_current_stage,
+    input                   stall_next_stage,
+    input   [WIDTH - 1:0]   in,
+    output  [WIDTH - 1:0]   out
+);
+
+    reg [WIDTH - 1:0] last_status;
+    assign out = last_status;
+
+    always @(posedge clk) begin
+        if (!rst) begin
+            last_status <= 0;
+        end
+        else if (flush) begin
+            last_status <= 0;
+        end
+        else if (stall_current_stage && !stall_next_stage) begin
+            last_status <= 0;
+        end
+        else if (!stall_current_stage) begin
+            last_status <= in;
+        end
+    end
+
+endmodule // PipelineDeliver
