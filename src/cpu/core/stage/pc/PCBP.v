39431feb746feabf4fc32be21b2d842f00ac41c8
MaxXSoft <x@MaxXSoft.net>
2019-05-30 18:11:42 +0800

    (PCBP) added mid-stage 'PCBP'

---
+++ src/cpu/core/stage/pc/PCBP.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+`include "bus.v"
+
+module PCBP(
+  input                 clk,
+  input                 rst,
+  input                 flush,
+  input                 stall_current_stage,
+  input                 stall_next_stage,
+  input   [`ADDR_BUS]   pc_in,
+  output  [`ADDR_BUS]   pc_out
+);
+
+  PipelineDeliver #(`ADDR_BUS_WIDTH) ff_pc(
+    clk, rst, flush,
+    stall_current_stage, stall_next_stage,
+    pc_in, pc_out
+  );
+
+endmodule // PCBP
