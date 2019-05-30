1e17ec4e764721c4501d99fd2957a9d68d04beb4
MaxXSoft <x@MaxXSoft.net>
2019-05-30 19:50:22 +0800

    (PC) modified some details

--- src/cpu/core/stage/pc/PC.v
+++ src/cpu/core/stage/pc/PC.v
@@ -4,23 +4,11 @@
 `include "exception.v"
 
 module PC(
-  input               clk,
   input               rst,
   input   [`ADDR_BUS] next_pc,
   output  [`ADDR_BUS] pc_out
 );
 
-  // PC register
-  reg [`ADDR_BUS] pc;
-  assign pc_out = pc;
-
-  always @(posedge clk) begin
-    if (!rst) begin
-      pc <= `INIT_PC;
-    end
-    else begin
-      pc <= next_pc;
-    end
-  end
+  assign pc_out = !rst ? `INIT_PC : next_pc;
 
 endmodule // PC
