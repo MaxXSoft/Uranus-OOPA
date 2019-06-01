3e6bc70d6eb1f0faac8ccdd60820f10ecb17aa8c
MaxXSoft <x@MaxXSoft.net>
2019-06-02 00:17:46 +0800

    (PC_tb) modified some details

--- test/stage/PC_tb.v
+++ test/stage/PC_tb.v
@@ -1,6 +1,7 @@
 `timescale 1ns / 1ps
 
 `include "tb.v"
+`include "bus.v"
 `include "branch.v"
 
 `define ADD_BRANCH_CHECK(src, dst, jump, taken)               \
@@ -9,10 +10,11 @@
       is_branch_in <= 1;                                      \
       is_jump_in <= jump;                                     \
       is_taken_in <= taken;                                   \
-      is_miss_in <= 1;                                        \
       last_pht_index <= mod_pht_index;                        \
       inst_pc <= mod_pc;                                      \
       target_in <= dst;                                       \
+      flush <= 1;                                             \
+      exc_pc <= dst;                                          \
     end
 
 module PC_tb(
@@ -24,16 +26,16 @@ module PC_tb(
   `GEN_TICK(clk, rst);
 
   // modules
-  reg is_branch_in, is_jump_in, is_taken_in, is_miss_in;
+  reg is_branch_in, is_jump_in, is_taken_in, flush;
   reg[`GHR_BUS] last_pht_index;
-  reg[31:0] inst_pc, target_in;
+  reg[`ADDR_BUS] inst_pc, target_in, exc_pc;
   wire is_branch_taken;
   wire[`GHR_BUS] pht_index_out;
-  wire[31:0] pc_out;
+  wire[`ADDR_BUS] pc_out;
 
   wire mod_is_branch_taken;
   wire[`GHR_BUS] mod_pht_index;
-  wire[31:0] mod_pc;
+  wire[`ADDR_BUS] mod_pc;
 
   PC stage_pc(
     .clk                  (clk),
@@ -42,14 +44,13 @@ module PC_tb(
     .is_branch_in         (is_branch_in),
     .is_jump_in           (is_jump_in),
     .is_taken_in          (is_taken_in),
-    .is_miss_in           (is_miss_in),
     .last_pht_index       (last_pht_index),
     .inst_pc              (inst_pc),
     .target_in            (target_in),
 
-    .flush                (0),
+    .flush                (flush),
     .stall                (0),
-    .exc_pc               (0),
+    .exc_pc               (exc_pc),
 
     .is_branch_taken      (is_branch_taken),
     .pht_index_out        (pht_index_out),
@@ -59,7 +60,7 @@ module PC_tb(
   PCIF pcif_mid(
     .clk                  (clk),
     .rst                  (rst),
-    .flush                (0),
+    .flush                (flush),
     .stall_current_stage  (0),
     .stall_next_stage     (0),
     .is_branch_taken_in   (is_branch_taken),
@@ -71,14 +72,15 @@ module PC_tb(
   );
 
   // testbench
-  always @(posedge clk) begin
+  always @(*) begin
     is_branch_in <= 0;
     is_jump_in <= 0;
     is_taken_in <= 0;
-    is_miss_in <= 0;
     last_pht_index <= 0;
     inst_pc <= 0;
     target_in <= 0;
+    flush <= 0;
+    exc_pc <= 0;
 
     `ADD_BRANCH_CHECK(32'hbfc00010, 32'hbfc00000, 0, 1);
 
