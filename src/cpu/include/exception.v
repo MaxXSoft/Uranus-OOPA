d582c589d1103e3b7ba97044c2abdbf731777bbb
MaxXSoft <x@MaxXSoft.net>
2019-06-02 00:17:19 +0800

    (exception) added macro 'INVALID_PC'

--- src/cpu/include/exception.v
+++ src/cpu/include/exception.v
@@ -6,4 +6,7 @@
 `define EXC_BASE            32'hbfc00200
 `define EXC_OFFSET          32'h00000180
 
+// invalid PC value in order to invalidate PC stage
+`define INVALID_PC        32'hffffffff
+
 `endif  // OOPA_CPU_INCLUDE_EXCEPTION_V_
