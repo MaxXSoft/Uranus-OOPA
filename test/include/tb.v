2613bcc6dbade0231d924f81c0240d4b2bce2e35
MaxXSoft <x@MaxXSoft.net>
2019-06-01 15:45:40 +0800

    (tb.v) modified some details

--- test/include/tb.v
+++ test/include/tb.v
@@ -1,18 +1,20 @@
 `ifndef OOPA_TEST_INCLUDE_TB_V_
 `define OOPA_TEST_INCLUDE_TB_V_
 
+`define TICK  _tb_tick
+
 `define GEN_TICK(clk, rst)              \
-    integer tick;                       \
+    integer `TICK;                      \
     always @(posedge clk) begin         \
       if (!rst) begin                   \
-        tick <= 0;                      \
+        `TICK <= 0;                     \
       end                               \
       else begin                        \
-        tick <= tick + 1;               \
+        `TICK <= `TICK + 1;             \
       end                               \
     end
 
 `define DISPLAY(name, val) \
-    $display("[%8h] %s = 0x%8h", tick, name, val)
+    $display("[%8h] %s = 0x%8h", `TICK, name, val)
 
 `endif  // OOPA_TEST_INCLUDE_TB_V_
