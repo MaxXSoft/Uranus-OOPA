c701923e83477dc16942a6a50b1765af7de2f160
MaxXSoft <x@MaxXSoft.net>
2019-06-02 09:57:53 +0800

    (InstCache, PipelineDeliver) fixed some details

--- src/cpu/cache/InstCache.v
+++ src/cpu/cache/InstCache.v
@@ -1,64 +1,65 @@
 `timescale 1ns / 1ps
 
 module InstCache #(parameter
-  ADDR_WIDTH = 32,
-  LINE_WIDTH = 6,         // 2^6 = 64 bytes/line
-  CACHE_WIDTH = 6         // 2^6 = 64 lines
-  `define LINE_COUNT      (2 ** CACHE_WIDTH)
-  `define INDEX_WIDTH     (LINE_WIDTH - 2)
-  `define TAG_WIDTH       (ADDR_WIDTH - LINE_WIDTH - CACHE_WIDTH)
+  kAddrWidth = 32,
+  kLineWidth = 6,         // 2^6 = 64 bytes/line
+  kCacheWidth = 6         // 2^6 = 64 lines
 ) (
-  input clk,
-  input rst,
+  input                       clk,
+  input                       rst,
   // cache control
-  input read_en,
-  input hit_invalidate,
-  input [ADDR_WIDTH - 1:0] addr_read,
-  input [ADDR_WIDTH - 1:0] addr_inv,
-  output ready,
-  output [31:0] data_out,
+  input                       read_en,
+  input                       hit_invalidate,
+  input   [kAddrWidth - 1:0]  addr_read,
+  input   [kAddrWidth - 1:0]  addr_inv,
+  output                      ready,
+  output  [31:0]              data_out,
   // AXI interface
-  output [3:0]  arid,
-  output [31:0] araddr,
-  output [7:0]  arlen,
-  output [2:0]  arsize,
-  output [1:0]  arburst,
-  output [1:0]  arlock,
-  output [3:0]  arcache,
-  output [2:0]  arprot,
-  output        arvalid,
-  input         arready,
+  output  [3:0]               arid,
+  output  [31:0]              araddr,
+  output  [7:0]               arlen,
+  output  [2:0]               arsize,
+  output  [1:0]               arburst,
+  output  [1:0]               arlock,
+  output  [3:0]               arcache,
+  output  [2:0]               arprot,
+  output                      arvalid,
+  input                       arready,
   // ---
-  input  [3:0]  rid,
-  input  [31:0] rdata,
-  input  [1:0]  rresp,
-  input         rlast,
-  input         rvalid,
-  output        rready,
+  input   [3:0]               rid,
+  input   [31:0]              rdata,
+  input   [1:0]               rresp,
+  input                       rlast,
+  input                       rvalid,
+  output                      rready,
   // ---
-  output [3:0]  awid,
-  output [31:0] awaddr,
-  output [7:0]  awlen,
-  output [2:0]  awsize,
-  output [1:0]  awburst,
-  output [1:0]  awlock,
-  output [3:0]  awcache,
-  output [2:0]  awprot,
-  output        awvalid,
-  input         awready,
+  output  [3:0]               awid,
+  output  [31:0]              awaddr,
+  output  [7:0]               awlen,
+  output  [2:0]               awsize,
+  output  [1:0]               awburst,
+  output  [1:0]               awlock,
+  output  [3:0]               awcache,
+  output  [2:0]               awprot,
+  output                      awvalid,
+  input                       awready,
   // ---
-  output [3:0]  wid,
-  output [31:0] wdata,
-  output [3:0]  wstrb,
-  output        wlast,
-  output        wvalid,
-  input         wready,
+  output  [3:0]               wid,
+  output  [31:0]              wdata,
+  output  [3:0]               wstrb,
+  output                      wlast,
+  output                      wvalid,
+  input                       wready,
   // ---
-  input  [3:0]  bid,
-  input  [1:0]  bresp,
-  input         bvalid,
-  output        bready
+  input   [3:0]               bid,
+  input   [1:0]               bresp,
+  input                       bvalid,
+  output                      bready
 );
+
+  localparam kLineCount = 2 ** kCacheWidth;
+  localparam kIndexWidth = kLineWidth - 2;
+  localparam kTagWidth = kAddrWidth - kLineWidth - kCacheWidth;
 
   // TODO
   // reference: Uranus Zero's unfinished instruction cache module
--- src/cpu/core/utility/PipelineDeliver.v
+++ src/cpu/core/utility/PipelineDeliver.v
@@ -1,30 +1,30 @@
 `timescale 1ns / 1ps
 
 module PipelineDeliver #(parameter
-  WIDTH = 1,
-  RST_VAL = 0
+  kWidth = 1,
+  kRstVal = 0
 ) (
-  input                 clk,
-  input                 rst,
-  input                 flush,
-  input                 stall_current_stage,
-  input                 stall_next_stage,
-  input   [WIDTH - 1:0] in,
-  output  [WIDTH - 1:0] out
+  input                   clk,
+  input                   rst,
+  input                   flush,
+  input                   stall_current_stage,
+  input                   stall_next_stage,
+  input   [kWidth - 1:0]  in,
+  output  [kWidth - 1:0]  out
 );
 
-  reg[WIDTH - 1:0] last_status;
+  reg[kWidth - 1:0] last_status;
   assign out = last_status;
 
   always @(posedge clk) begin
     if (!rst) begin
-      last_status <= RST_VAL;
+      last_status <= kRstVal;
     end
     else if (flush) begin
-      last_status <= RST_VAL;
+      last_status <= kRstVal;
     end
     else if (stall_current_stage && !stall_next_stage) begin
-      last_status <= RST_VAL;
+      last_status <= kRstVal;
     end
     else if (!stall_current_stage) begin
       last_status <= in;
