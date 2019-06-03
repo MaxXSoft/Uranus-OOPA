3701b91de7ceea8a6334515335a17e122eaedd18
MaxXSoft <x@MaxXSoft.net>
2019-06-04 00:06:59 +0800

    (rob) added header 'rob.v'

---
+++ src/cpu/include/rob.v
@@ -0,0 +1,35 @@
+`ifndef OOPA_CPU_INCLUDE_ROB_V_
+`define OOPA_CPU_INCLUDE_ROB_V_
+
+`include "util.v"
+
+// --- reservation station config ---
+
+// integer unit
+`define RS_INT_ADDR_WIDTH     4
+`define RS_INT_SIZE           `MAKE_SIZE(`RS_INT_ADDR_WIDTH)
+
+// div unit
+`define RS_DIV_ADDR_WIDTH     4
+`define RS_DIV_SIZE           `MAKE_SIZE(`RS_MULDIV_ADDR_WIDTH)
+
+// floating point unit
+`define RS_FPU_ADDR_WIDTH     4
+`define RS_FPU_SIZE           `MAKE_SIZE(`RS_FPU_ADDR_WIDTH)
+
+// load store unit
+`define RS_LSU_ADDR_WIDTH     4
+`define RS_LSU_SIZE           `MAKE_SIZE(`RS_LSU_ADDR_WIDTH)
+
+// ---------   end config   ---------
+
+
+// reservation station id
+`define RSID_WIDTH            6
+`define RSID_BUS              `MAKE_BUS(`RSID_WIDTH)
+
+// reorder buffer config
+`define ROB_ADDR_WIDTH        6
+`define ROB_SIZE              `MAKE_SIZE(`ROB_ADDR_WIDTH)
+
+`endif  // OOPA_CPU_INCLUDE_ROB_V_
