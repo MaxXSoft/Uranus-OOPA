939144f551b0e948799e1bbda88cfdb406a20fb3
MaxXSoft <x@MaxXSoft.net>
2019-06-03 23:55:50 +0800

    (util) added header 'util.v'

---
+++ src/cpu/include/util.v
@@ -0,0 +1,9 @@
+`ifndef OOPA_CPU_INCLUDE_ROB_V_
+`define OOPA_CPU_INCLUDE_ROB_V_
+
+// some global utilities
+
+`define MAKE_SIZE(width)    (2 ** (width))
+`define MAKE_BUS(width)     (width) - 1:0
+
+`endif  // OOPA_CPU_INCLUDE_ROB_V_
