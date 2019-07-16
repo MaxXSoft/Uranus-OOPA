bab864e823516491fbec810f7700519ec82c6edb
MaxXSoft <x@MaxXSoft.net>
2019-07-16 18:45:16 +0800

    (include) added some new headers about decoding

---
+++ src/cpu/include/regimm.v
@@ -0,0 +1,10 @@
+`ifndef OOPA_CPU_INCLUDE_REGIMM_V_
+`define OOPA_CPU_INCLUDE_REGIMM_V_
+
+// some branch instructions
+`define REGIMM_BLTZ     5'b00000
+`define REGIMM_BLTZAL   5'b10000
+`define REGIMM_BGEZ     5'b00001
+`define REGIMM_BGEZAL   5'b10001
+
+`endif  // OOPA_CPU_INCLUDE_REGIMM_V_
