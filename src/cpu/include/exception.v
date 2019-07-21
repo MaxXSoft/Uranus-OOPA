ec461f02db24c4cb58a8e8c038c6621c174367b3
MaxXSoft <x@MaxXSoft.net>
2019-07-21 20:31:17 +0800

    (exception) updated header 'exception.v'

--- src/cpu/include/exception.v
+++ src/cpu/include/exception.v
@@ -9,4 +9,28 @@
 // invalid PC value in order to invalidate PC stage
 `define INVALID_PC          32'hffffffff
 
+// exception type segment position
+`define EXC_TYPE_POS_INT    7:0
+`define EXC_TYPE_POS_IF     0
+`define EXC_TYPE_POS_RI     1
+`define EXC_TYPE_POS_OV     2
+`define EXC_TYPE_POS_TP     3
+`define EXC_TYPE_POS_BP     4
+`define EXC_TYPE_POS_SYS    5
+`define EXC_TYPE_POS_ADE    6   // NOTE: can be removed
+`define EXC_TYPE_POS_ERET   7
+
+// exception type definitions
+`define EXC_TYPE_NULL       8'h0
+`define EXC_TYPE_INT        8'h1
+`define EXC_TYPE_IF         8'h2
+`define EXC_TYPE_RI         8'h3
+`define EXC_TYPE_OV         8'h4
+`define EXC_TYPE_TP         8'h5
+`define EXC_TYPE_BP         8'h6
+`define EXC_TYPE_SYS        8'h7
+`define EXC_TYPE_ADEL       8'h8
+`define EXC_TYPE_ADES       8'h9
+`define EXC_TYPE_ERET       8'ha
+
 `endif  // OOPA_CPU_INCLUDE_EXCEPTION_V_
