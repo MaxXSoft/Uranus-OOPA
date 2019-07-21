dfacb8f2eba0ca876178a48b2b4d96916fa6801b
MaxXSoft <x@MaxXSoft.net>
2019-07-21 11:59:02 +0800

    (ID, IDROB) updated stage 'ID'

--- src/cpu/core/stage/id/IDROB.v
+++ src/cpu/core/stage/id/IDROB.v
@@ -2,6 +2,7 @@
 
 `include "bus.v"
 `include "branch.v"
+`include "opgen.v"
 
 module IDROB(
   input                   clk,
@@ -13,26 +14,20 @@ module IDROB(
   input   [`REG_ADDR_BUS] reg_write_addr_in,
   input                   is_branch_taken_in,
   input   [`GHR_BUS]      pht_index_in,
-  input                   is_inst_branch_in,
-  input                   is_inst_jump_in,
-  input                   is_inst_branch_taken_in,
-  input                   is_inst_branch_determined_in,
   input   [`ADDR_BUS]     inst_branch_target_in,
-  input                   is_next_delayslot_in,
-  input                   is_delayslot_in,
   input                   mem_write_flag_in,
   input                   mem_read_flag_in,
   input                   mem_sign_ext_flag_in,
   input   [3:0]           mem_sel_in,
   input                   mem_write_is_ref_in,
-  input   [`DATA_BUS]     mem_write_data_in,
-  input   [`CP0_ADDR_BUS] cp0_addr_in,
+  input   [`DATA_BUS]     mem_offset_in,
   input                   cp0_read_flag_in,
   input                   cp0_write_flag_in,
-  input                   cp0_write_is_ref_in,
-  input   [`DATA_BUS]     cp0_write_data_in,
+  input   [`CP0_ADDR_BUS] cp0_addr_in,
   input   [`EXC_TYPE_BUS] exception_type_in,
-  input   [`FUNCT_BUS]    funct_in,
+  input                   is_next_delayslot_in,
+  input                   is_delayslot_in,
+  input   [`OPGEN_BUS]    opgen_in,
   input   [`SHAMT_BUS]    shamt_in,
   input                   operand_is_ref_1_in,
   input                   operand_is_ref_2_in,
@@ -43,26 +38,19 @@ module IDROB(
   output  [`REG_ADDR_BUS] reg_write_addr_out,
   output                  is_branch_taken_out,
   output  [`GHR_BUS]      pht_index_out,
-  output                  is_inst_branch_out,
-  output                  is_inst_jump_out,
-  output                  is_inst_branch_taken_out,
-  output                  is_inst_branch_determined_out,
   output  [`ADDR_BUS]     inst_branch_target_out,
-  output                  is_current_delayslot_out,
-  output                  is_delayslot_out,
   output                  mem_write_flag_out,
   output                  mem_read_flag_out,
   output                  mem_sign_ext_flag_out,
   output  [3:0]           mem_sel_out,
-  output                  mem_write_is_ref_out,
-  output  [`DATA_BUS]     mem_write_data_out,
-  output  [`CP0_ADDR_BUS] cp0_addr_out,
+  output  [`DATA_BUS]     mem_offset_out,
   output                  cp0_read_flag_out,
   output                  cp0_write_flag_out,
-  output                  cp0_write_is_ref_out,
-  output  [`DATA_BUS]     cp0_write_data_out,
+  output  [`CP0_ADDR_BUS] cp0_addr_out,
   output  [`EXC_TYPE_BUS] exception_type_out,
-  output  [`FUNCT_BUS]    funct_out,
+  output                  is_current_delayslot_out,
+  output                  is_delayslot_out,
+  output  [`OPGEN_BUS]    opgen_out,
   output  [`SHAMT_BUS]    shamt_out,
   output                  operand_is_ref_1_out,
   output                  operand_is_ref_2_out,
@@ -95,46 +83,10 @@ module IDROB(
     pht_index_in, pht_index_out
   );
 
-  PipelineDeliver #(1) ff_is_inst_branch(
-    clk, rst, flush,
-    stall_current_stage, stall_next_stage,
-    is_inst_branch_in, is_inst_branch_out
-  );
-
-  PipelineDeliver #(1) ff_is_inst_jump(
-    clk, rst, flush,
-    stall_current_stage, stall_next_stage,
-    is_inst_jump_in, is_inst_jump_out
-  );
-
-  PipelineDeliver #(1) ff_is_inst_branch_taken(
-    clk, rst, flush,
-    stall_current_stage, stall_next_stage,
-    is_inst_branch_taken_in, is_inst_branch_taken_out
-  );
-
-  PipelineDeliver #(1) ff_is_inst_branch_determined(
-    clk, rst, flush,
-    stall_current_stage, stall_next_stage,
-    is_inst_branch_determined_in, is_inst_branch_determined_out
-  );
-
   PipelineDeliver #(`ADDR_BUS_WIDTH) ff_inst_branch_target(
     clk, rst, flush,
     stall_current_stage, stall_next_stage,
     inst_branch_target_in, inst_branch_target_out
-  );
-
-  PipelineDeliver #(1) ff_is_current_delayslot(
-    clk, rst, flush,
-    stall_current_stage, stall_next_stage,
-    is_next_delayslot_in, is_current_delayslot_out
-  );
-
-  PipelineDeliver #(1) ff_is_delayslot(
-    clk, rst, flush,
-    stall_current_stage, stall_next_stage,
-    is_delayslot_in, is_delayslot_out
   );
 
   PipelineDeliver #(1) ff_mem_write_flag(
@@ -161,22 +113,10 @@ module IDROB(
     mem_sel_in, mem_sel_out
   );
 
-  PipelineDeliver #(1) ff_mem_write_is_ref(
+  PipelineDeliver #(`DATA_BUS_WIDTH) ff_mem_offset(
     clk, rst, flush,
     stall_current_stage, stall_next_stage,
-    mem_write_is_ref_in, mem_write_is_ref_out
-  );
-
-  PipelineDeliver #(`DATA_BUS_WIDTH) ff_mem_write_data(
-    clk, rst, flush,
-    stall_current_stage, stall_next_stage,
-    mem_write_data_in, mem_write_data_out
-  );
-
-  PipelineDeliver #(`CP0_ADDR_BUS_WIDTH) ff_cp0_addr(
-    clk, rst, flush,
-    stall_current_stage, stall_next_stage,
-    cp0_addr_in, cp0_addr_out
+    mem_offset_in, mem_offset_out
   );
 
   PipelineDeliver #(1) ff_cp0_read_flag(
@@ -191,16 +131,10 @@ module IDROB(
     cp0_write_flag_in, cp0_write_flag_out
   );
 
-  PipelineDeliver #(1) ff_cp0_write_is_ref(
+  PipelineDeliver #(`CP0_ADDR_BUS_WIDTH) ff_cp0_addr(
     clk, rst, flush,
     stall_current_stage, stall_next_stage,
-    cp0_write_is_ref_in, cp0_write_is_ref_out
-  );
-
-  PipelineDeliver #(`DATA_BUS_WIDTH) ff_cp0_write_data(
-    clk, rst, flush,
-    stall_current_stage, stall_next_stage,
-    cp0_write_data_in, cp0_write_data_out
+    cp0_addr_in, cp0_addr_out
   );
 
   PipelineDeliver #(`EXC_TYPE_BUS_WIDTH) ff_exception_type(
@@ -209,10 +143,22 @@ module IDROB(
     exception_type_in, exception_type_out
   );
 
-  PipelineDeliver #(`FUNCT_BUS_WIDTH) ff_funct(
+  PipelineDeliver #(1) ff_is_current_delayslot(
     clk, rst, flush,
     stall_current_stage, stall_next_stage,
-    funct_in, funct_out
+    is_next_delayslot_in, is_current_delayslot_out
+  );
+
+  PipelineDeliver #(1) ff_is_delayslot(
+    clk, rst, flush,
+    stall_current_stage, stall_next_stage,
+    is_delayslot_in, is_delayslot_out
+  );
+
+  PipelineDeliver #(`OPGEN_WIDTH) ff_opgen(
+    clk, rst, flush,
+    stall_current_stage, stall_next_stage,
+    opgen_in, opgen_out
   );
 
   PipelineDeliver #(`SHAMT_BUS_WIDTH) ff_shamt(
