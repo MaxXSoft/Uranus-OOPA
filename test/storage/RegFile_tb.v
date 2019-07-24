aea2a4a85304dc0f09e10dabd2907a6871d9e92c
MaxXSoft <x@MaxXSoft.net>
2019-07-24 11:42:02 +0800

    (RegFile_tb) updated testbench

--- test/storage/RegFile_tb.v
+++ test/storage/RegFile_tb.v
@@ -3,6 +3,8 @@
 `include "tb.v"
 `include "bus.v"
 `include "rob.v"
+`include "regfile.v"
+`include "exception.v"
 
 module RegFile_tb(
   input clk,
@@ -13,33 +15,40 @@ module RegFile_tb(
 
   // input signals
   reg                 write_en;
-  reg[`REG_ADDR_BUS]  write_addr;
+  reg[`RF_ADDR_BUS]   write_addr;
   reg[`ROB_ADDR_BUS]  write_ref_id;
   reg                 commit_en;
   reg                 commit_restore;
-  reg[`REG_ADDR_BUS]  commit_addr;
+  reg[`RF_ADDR_BUS]   commit_addr;
   reg[`DATA_BUS]      commit_data;
   reg                 read_en_1;
   reg                 read_en_2;
-  reg[`REG_ADDR_BUS]  read_addr_1;
-  reg[`REG_ADDR_BUS]  read_addr_2;
+  reg[`RF_ADDR_BUS]   read_addr_1;
+  reg[`RF_ADDR_BUS]   read_addr_2;
 
   // output signals
   wire                read_is_ref_1;
   wire                read_is_ref_2;
   wire[`DATA_BUS]     read_data_1;
   wire[`DATA_BUS]     read_data_2;
+  wire[`DATA_BUS]     cp0_status;
+  wire[`DATA_BUS]     cp0_cause;
+  wire[`DATA_BUS]     cp0_epc;
+  wire[`DATA_BUS]     cp0_ebase;
 
   RegFile regfile(
     .clk            (clk),
     .rst            (rst),
+
     .write_en       (write_en),
     .write_addr     (write_addr),
     .write_ref_id   (write_ref_id),
+
     .commit_en      (commit_en),
     .commit_restore (commit_restore),
     .commit_addr    (commit_addr),
     .commit_data    (commit_data),
+
     .read_en_1      (read_en_1),
     .read_en_2      (read_en_2),
     .read_addr_1    (read_addr_1),
@@ -47,7 +56,18 @@ module RegFile_tb(
     .read_is_ref_1  (read_is_ref_1),
     .read_is_ref_2  (read_is_ref_2),
     .read_data_1    (read_data_1),
-    .read_data_2    (read_data_2)
+    .read_data_2    (read_data_2),
+
+    .hard_int       (0),
+    .badvaddr_data  (0),
+    .exception_type (0),
+    .is_delayslot   (0),
+    .current_pc     (0),
+
+    .cp0_status     (cp0_status),
+    .cp0_cause      (cp0_cause),
+    .cp0_epc        (cp0_epc),
+    .cp0_ebase      (cp0_ebase)
   );
 
   always @(posedge clk) begin
@@ -118,6 +138,10 @@ module RegFile_tb(
     `DISPLAY("read_is_ref_2 ", read_is_ref_2);
     `DISPLAY("read_data_1   ", read_data_1);
     `DISPLAY("read_data_2   ", read_data_2);
+    `DISPLAY("cp0_status    ", cp0_status);
+    `DISPLAY("cp0_cause     ", cp0_cause);
+    `DISPLAY("cp0_epc       ", cp0_epc);
+    `DISPLAY("cp0_ebase     ", cp0_ebase);
     $display("");
     `END_AT_TICK(4);
   end
