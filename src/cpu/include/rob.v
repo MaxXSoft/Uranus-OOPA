2aa77a5ed3a537c0ac2249cd52a65b211ec0966d
MaxXSoft <x@MaxXSoft.net>
2019-07-25 14:02:38 +0800

    (rob) removed 'COU' function unit

--- src/cpu/include/rob.v
+++ src/cpu/include/rob.v
@@ -6,28 +6,24 @@
 // --- reservation station config ---
 
 // integer unit
-`define RS_INT_ADDR_WIDTH     3
+`define RS_INT_ADDR_WIDTH     4
 `define RS_INT_SIZE           `MAKE_SIZE(`RS_INT_ADDR_WIDTH)
 
 // mult div unit
-`define RS_MDU_ADDR_WIDTH     3
+`define RS_MDU_ADDR_WIDTH     4
 `define RS_MDU_SIZE           `MAKE_SIZE(`RS_MDU_ADDR_WIDTH)
 
-// floating point unit
-`define RS_FPU_ADDR_WIDTH     3
-`define RS_FPU_SIZE           `MAKE_SIZE(`RS_FPU_ADDR_WIDTH)
+// // floating point unit
+// `define RS_FPU_ADDR_WIDTH     3
+// `define RS_FPU_SIZE           `MAKE_SIZE(`RS_FPU_ADDR_WIDTH)
 
 // load store unit
-`define RS_LSU_ADDR_WIDTH     3
+`define RS_LSU_ADDR_WIDTH     4
 `define RS_LSU_SIZE           `MAKE_SIZE(`RS_LSU_ADDR_WIDTH)
 
 // branch unit
-`define RS_BRU_ADDR_WIDTH     3
+`define RS_BRU_ADDR_WIDTH     4
 `define RS_BRU_SIZE           `MAKE_SIZE(`RS_BRU_ADDR_WIDTH)
-
-// co-processor unit
-`define RS_COU_ADDR_WIDTH     3
-`define RS_COU_SIZE           `MAKE_SIZE(`RS_COU_ADDR_WIDTH)
 
 // ---------   end config   ---------
 
